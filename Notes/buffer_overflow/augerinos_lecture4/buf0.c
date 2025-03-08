16cdddda2a1dbd7e2550c366dbc4e534aee6d88c
AlexTuring <alexgkiafi@gmail.com>
2025-03-08 23:01:35 +0200

    added some notes

---
+++ Notes/buffer_overflow/augerinos_lecture4/buf0.c
@@ -0,0 +1,26 @@
+#include <string.h>
+#include <unistd.h>
+#include <stdio.h>
+
+void thank_you()
+{
+    execl("/bin/sh", "sh", NULL);
+}
+
+void print(char *message)
+{
+    char buffer[64];
+    strcpy(buffer, message);
+    printf("%s\n", buffer);
+}
+
+int main(int argc, char *argv[])
+{
+    if (argc != 2)
+    {
+        printf("Usage: %s <name>\n", argv[0]);
+        return 1;
+    }
+    print(argv[1]);
+    return 0;
+}
---
+++ Notes/strace/strace_notes.md
@@ -0,0 +1,1 @@
+https://www.youtube.com/watch?v=quQmdgIUCaU&list=PL_jb7LxOF7HULE7n1ksAt0HEPMxNAXZ8j
